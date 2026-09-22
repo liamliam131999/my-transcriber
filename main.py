9fd633d0a27122fcad5a4d402d9c5f17923144fd
liamliam131999 <liamliam.jin@gmail.com>
2026-09-22 08:07:01 +0630

    Refactor server initialization in main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 from http.server import HTTPServer
 from api.download import handler
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    server = HTTPServer(("0.0.0.0", port), handler)
-    print(f"Server running on port {port}")
-    server.serve_forever()
+port = int(os.environ.get("PORT", 8000))
+
+server = HTTPServer(("0.0.0.0", port), handler)
+
+print(f"Server running on port {port}", flush=True)
+
+server.serve_forever()
