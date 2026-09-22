0b3bd665b750d1528d6179baedf9fa0d872c43a3
liamliam131999 <liamliam.jin@gmail.com>
2026-09-22 07:53:11 +0630

    Refactor import and remove main execution block

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import os
 from http.server import HTTPServer
-from api.download import *
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    HTTPServer(("0.0.0.0", port), Handler).serve_forever()
+from api.download import Handler
