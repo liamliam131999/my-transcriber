6e69ee5eb97b79c3598dea58580363636a40764b
liamliam131999 <liamliam.jin@gmail.com>
2026-09-22 07:55:36 +0630

    Add main entry point for HTTP server

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import os
 from http.server import HTTPServer
 from api.download import Handler
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    HTTPServer(("0.0.0.0", port), Handler).serve_forever()
